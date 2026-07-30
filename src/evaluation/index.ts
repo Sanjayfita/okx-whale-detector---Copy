edeca1ad76bf51658d72d4d96869170fbb46cc4b
Sanjayfita <ellan15jude@gmail.com>
2026-07-30 14:29:24 +0800

    Export unified alert quality report persistence

--- src/evaluation/index.ts
+++ src/evaluation/index.ts
@@ -11,6 +11,7 @@ export * from './alertQualityStatistics';
 export * from './alertQualityTargetStopAggregator';
 export * from './alertQualityTerminalReturnAggregator';
 export * from './alertQualityUnifiedReport';
+export * from './alertQualityUnifiedReportPersistence';
 export * from './canonicalJson';
 export * from './candleAlignment';
 export * from './candleNormalization';
